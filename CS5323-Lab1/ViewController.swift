378cf43e1fd980792ad586cd2626b29c0e3e5dd1
Nick Wall <60442663+nicholaswall@users.noreply.github.com>
2022-09-07 10:49:41 -0500

    Initial Commit

---
+++ CS5323-Lab1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CS5323-Lab1
+//
+//  Created by Nick Wall on 9/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
